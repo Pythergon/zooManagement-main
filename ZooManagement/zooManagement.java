4b38945a36c743a3fb1f114808c2fc317776cbd8
Pythergon <pythonissupreme@gmail.com>
2025-07-19 20:02:18 -0500

    modified:   ZooManagement/zooManagement.java

--- ZooManagement/zooManagement.java
+++ ZooManagement/zooManagement.java
@@ -1,11 +1,14 @@
 public class zooManagement {
     public static void main(String[] args) {
+
+        // Object declarations!
         animal Romeo = new animal("Romeo", 4, "Horse");
         animal Spankey = new animal("Spankey", 5, "Donkey");
         animal Bambi = new animal("Bambi", 2, "Deer");
         container Conatainer1 = new container(2);
         food Hay = new food("Hay",10);
 
+        // Object interactions!
         Spankey.hungerLevel = 2;
         Spankey.containAnimal(Conatainer1, Spankey);
         Romeo.containAnimal(Conatainer1, Romeo);
